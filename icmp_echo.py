e3162d22de57373f4332f4c368b46c211f9e7f51
SaltyGrandma <nicholasklogan@gmail.com>
2021-02-01 01:28:39 -0500

    Updated tests to reflect actual full ping response
    >           return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split(" ")[4].split("=")[1]}
    E           IndexError: list index out of range

--- icmp_echo.py
+++ icmp_echo.py
@@ -3,7 +3,7 @@
 def main(ping_command):
     def true_main(ip_address):
         if (ping_response := ping_command()) != 'Request timed out.':
-            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split(" ")[4].split("=")[1]}
+            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(" ")[4].split("=")[1]}
         else:
             return {}
     return true_main
