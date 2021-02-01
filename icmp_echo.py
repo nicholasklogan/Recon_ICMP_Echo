5f7a3598b691f0614510685020daaea237bb2bd3
SaltyGrandma <nicholasklogan@gmail.com>
2021-02-01 01:50:56 -0500

    fixed to work with modified expected output to reflect actual ping response
    >           return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(' ')[4].split('=')[1]}

--- icmp_echo.py
+++ icmp_echo.py
@@ -3,6 +3,17 @@
 def main(ping_command):
     def true_main(ip_address):
         if 'Request timed out.' not in (ping_response := ping_command(ip_address)):
-            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(' ')[4].split('=')[1]}
+            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[2].split(' ')[4].split('=')[1]}
         return {}
     return true_main
+
+
+if __name__ == "__main__":
+    import subprocess
+
+    def ping(ip):
+        p = subprocess.Popen(['ping', '/n', '1', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        return stdout.decode('utf-8')
+
+    print(main(ping_command=ping)('8.8.8.8'))
