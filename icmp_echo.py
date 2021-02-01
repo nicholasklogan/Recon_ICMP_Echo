212d97ee73769acf36d4407c889fb9079d0aa179
SaltyGrandma <nicholasklogan@gmail.com>
2021-02-01 01:40:49 -0500

    fixed test to give more accurate call to dependency
    E       TypeError: ping() missing 1 required positional argument: 'ip'

--- icmp_echo.py
+++ icmp_echo.py
@@ -2,7 +2,7 @@
 
 def main(ping_command):
     def true_main(ip_address):
-        if 'Request timed out.' not in (ping_response := ping_command()):
+        if 'Request timed out.' not in (ping_response := ping_command(ip_address)):
             return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(' ')[4].split('=')[1]}
         return {}
     return true_main
