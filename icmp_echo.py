ad27131e2e2fb2d662f2ef49b674ed8de604bac3
SaltyGrandma <nicholasklogan@gmail.com>
2021-02-01 01:30:24 -0500

    refactor: minor

--- icmp_echo.py
+++ icmp_echo.py
@@ -3,7 +3,6 @@
 def main(ping_command):
     def true_main(ip_address):
         if 'Request timed out.' not in (ping_response := ping_command()):
-            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(" ")[4].split("=")[1]}
-        else:
-            return {}
+            return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_response.split('\n')[1].split(' ')[4].split('=')[1]}
+        return {}
     return true_main
