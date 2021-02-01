2c1759cc24e4abcccbd5a73fed3597abf9f16fea
SaltyGrandma <nicholasklogan@gmail.com>
2021-02-01 01:02:00 -0500

    Passing Test: test_slower_response_icmp_success_on_windows_with_slow_response_recorded
    E       AssertionError: assert {'TTL': 115, 'bytes': 32, 'ip': '8.8.8.8', 'time': '33ms'} == {'TTL': 115, 'bytes': 32, 'ip': '8.8.8.8', 'time': '330ms'}

--- icmp_echo.py
+++ icmp_echo.py
@@ -1,4 +1,7 @@
 
 
-def main(ip_address):
-    return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': '33ms'}
+def main(ping_command):
+    def true_main(ip_address):
+
+        return {'TTL': 115, 'bytes': 32, 'ip': ip_address, 'time': ping_command().split(" ")[4].split("=")[1]}
+    return true_main
